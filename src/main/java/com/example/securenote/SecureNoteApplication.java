73d56efcc5b81ce316d1c2f3885edfa5e4f013cd
woodgeon <ad4086@naver.com>
2025-10-21 13:28:41 +0900

    Initial commit

---
+++ src/main/java/com/example/securenote/SecureNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.securenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securenote/SecureNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
